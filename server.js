0fc2d1623bffa332862868dfc1b248925c05d91f
hamzashamoun96 <hamza.shamoun96@gmail.com>
2021-03-01 21:24:47 +0200

    lab12

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ require('dotenv').config();
 const cors = require('cors');
 server.use(cors());
 
+
+const pg = require('pg')
+// const client = new pg.Client(process.env.DATABASE_URL)
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+
 const superagent = require('superagent');
 
 const port = process.env.PORT || 4000
@@ -16,52 +21,86 @@ server.use(express.static('./public'));
 server.use(express.urlencoded({ extended: true }));
 server.set('view engine', 'ejs');
 
+// Routes------------------------------------------------------------------------------
 
-// Routes
-
-// localhost:3500/
-server.get('/', (req, res) => {
-    res.render('pages/index')
-})
-
-// http://localhost:3500/search?searchBook=flower&author=on
-server.get('/search', (req, res) => {
-    let BookName = req.query.searchBook;
-    let select = req.query.sort
-    //  res.redirect('pages/searches/show')
-    // if (req.query.title) {
-    //     select = 'intitle'
-    // } else if (req.query.author) {
-    //     select = 'inauthor'
-    // }
-
-
-    let url = `https://www.googleapis.com/books/v1/volumes?q=${BookName}+${select}:keyes`
-
-    superagent.get(url)
-        .then(bookData => {
-            let bookArr = bookData.body.items.map(val => {
-                return new Books(val)
-            })
-            res.render('pages/searches/show',{bookMenu : bookArr })
-        })
-        .catch((error)=>{
-            res.send('ERROR',error.message)
-        })
-
-})
-
-function Books(bookData) {
-    this.imgUrl = bookData.volumeInfo.imageLinks.smallThumbnail;
-    this.title = bookData.volumeInfo.title;
-    this.author = bookData.volumeInfo.authors;
-    this.description = bookData.volumeInfo.description
-}
-
-server.get('*',(req,res)=>{
+server.get('/', homePageHandler)              //  Home Page including the added books
+server.get('/searches', searchFormHandler)    //  Search Form
+server.post('/searches/new', infoHandler)     //  Search Results
+server.post('/addbook', addHandler)           //  Adding Books
+server.get('/books/:id', detailsHandler)      //  Viewing Books
+server.get('*',(req,res)=>{                   //  Wrong Route
     res.render('pages/error')
 })
 
-server.listen(port, () => {
-    console.log(`Listening to Port ${port}`)
-})
+// Route Handler Functions-------------------------------------------------------------- 
+
+function homePageHandler (req, res){
+    let SQL = `SELECT * FROM book;`
+    client.query(SQL)
+        .then(result => {
+            res.render('pages/index', { homeData: result.rows })
+        })
+    }
+
+function searchFormHandler (req, res) {
+    res.render('pages/searches/new');
+}
+
+function infoHandler (req, res)  {
+    let BookName = req.body.searchBook;
+    let select = req.body.sort
+    let url = `https://www.googleapis.com/books/v1/volumes?q=+${select}:${BookName}`
+
+    superagent.get(url)
+        .then(bookData => {
+            // console.log(bookData.body.items[0].volumeInfo.imageLinks.smallThumbnail)
+            let bookArr = bookData.body.items.map(val => {
+                return new Books(val)
+            })
+            res.render('pages/searches/show', { bookMenu: bookArr })
+        })
+}
+
+function addHandler (req, res){
+    let SQL = `INSERT INTO book(title,author,isbn,image_url,description) VALUES ($1,$2,$3,$4,$5)RETURNING id;`
+    let Body = req.body
+    let safeValues = [Body.title, Body.author, Body.isbn, Body.image_url, Body.description]
+
+    client.query(SQL, safeValues)
+        .then(() => {
+            res.redirect('/')
+        })
+}
+
+function detailsHandler (req, res){
+    // console.log(req.params)
+    let SQL = `SELECT * FROM book WHERE id=$1;`
+    let safeValue =[req.params.id]
+    console.log(safeValue)
+    client.query(SQL, safeValue)
+        .then(result => {
+            res.render('pages/books/detail', { book: result.rows[0]})
+        })
+}
+
+// Conctructor --------------------------------------------------------------------------------------
+
+function Books(bookdata) {
+    if (bookdata.volumeInfo.imageLinks !== undefined) {
+        this.imgUrl = bookdata.volumeInfo.imageLinks.smallThumbnail;
+    } else {
+        this.imgUrl = ' ';
+    }
+
+    this.title = bookdata.volumeInfo.title;
+    this.author = bookdata.volumeInfo.authors;
+    this.isbn = bookdata.volumeInfo.industryIdentifiers[0].type + bookdata.volumeInfo.industryIdentifiers[0].identifier
+    this.description = bookdata.volumeInfo.description
+}
+
+client.connect()
+    .then(() => {
+        server.listen(port, () => {
+            console.log(`Listening to Port ${port}`)
+        });
+    });
