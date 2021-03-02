3caaf637dff41ba6b946fa3030c5598ba1d58db8
hamzashamoun96 <hamza.shamoun96@gmail.com>
2021-03-02 18:40:20 +0200

    lab13

--- server.js
+++ server.js
@@ -10,10 +10,13 @@ server.use(cors());
 
 
 const pg = require('pg')
-// const client = new pg.Client(process.env.DATABASE_URL)
-const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+const client = new pg.Client(process.env.DATABASE_URL)
+// const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
 const superagent = require('superagent');
+
+const meethodOverride = require('method-override')
+server.use(meethodOverride('-method'))
 
 const port = process.env.PORT || 4000
 
@@ -28,25 +31,27 @@ server.get('/searches', searchFormHandler)    //  Search Form
 server.post('/searches/new', infoHandler)     //  Search Results
 server.post('/addbook', addHandler)           //  Adding Books
 server.get('/books/:id', detailsHandler)      //  Viewing Books
-server.get('*',(req,res)=>{                   //  Wrong Route
+server.put('/books/:id', updateHandler)       //  Update Books
+server.delete('/books/:id', deleteHandler)    //  Delete Books
+server.get('*', (req, res) => {               //  Wrong Route
     res.render('pages/error')
 })
 
 // Route Handler Functions-------------------------------------------------------------- 
 
-function homePageHandler (req, res){
+function homePageHandler(req, res) {
     let SQL = `SELECT * FROM book;`
     client.query(SQL)
         .then(result => {
-            res.render('pages/index', { homeData: result.rows })
+            res.render('pages/index', { homeData: result.rows, count: result.rows.length })
         })
-    }
+}
 
-function searchFormHandler (req, res) {
+function searchFormHandler(req, res) {
     res.render('pages/searches/new');
 }
 
-function infoHandler (req, res)  {
+function infoHandler(req, res) {
     let BookName = req.body.searchBook;
     let select = req.body.sort
     let url = `https://www.googleapis.com/books/v1/volumes?q=+${select}:${BookName}`
@@ -61,7 +66,7 @@ function infoHandler (req, res)  {
         })
 }
 
-function addHandler (req, res){
+function addHandler(req, res) {
     let SQL = `INSERT INTO book(title,author,isbn,image_url,description) VALUES ($1,$2,$3,$4,$5)RETURNING id;`
     let Body = req.body
     let safeValues = [Body.title, Body.author, Body.isbn, Body.image_url, Body.description]
@@ -72,16 +77,39 @@ function addHandler (req, res){
         })
 }
 
-function detailsHandler (req, res){
+function detailsHandler(req, res) {
     // console.log(req.params)
     let SQL = `SELECT * FROM book WHERE id=$1;`
-    let safeValue =[req.params.id]
-    console.log(safeValue)
+    let safeValue = [req.params.id]
     client.query(SQL, safeValue)
         .then(result => {
-            res.render('pages/books/detail', { book: result.rows[0]})
+            res.render('pages/books/detail', { book: result.rows[0] })
         })
 }
+
+function updateHandler(req, res) {
+    // console.log(req.params.id)
+    // console.log(req.body)
+    let { title, author, isbn, image_url, description } = req.body
+
+    let SQL = `UPDATE book SET title=$1,author=$2,isbn=$3,image_url=$4,description=$5 WHERE id=$6;`
+    let safeValues = [title, author, isbn, image_url, description, req.params.id]
+    client.query(SQL, safeValues)
+        .then(() => {
+            res.redirect(`/books/${req.params.id}`)
+        })
+}
+
+function deleteHandler(req, res) {
+    let SQL = `DELETE FROM book WHERE id=$1;`
+    let safevalue = [req.params.id]
+
+    client.query(SQL, safevalue)
+        .then(() => {
+            res.redirect('/');
+        })
+}
+
 
 // Conctructor --------------------------------------------------------------------------------------
 
@@ -94,7 +122,7 @@ function Books(bookdata) {
 
     this.title = bookdata.volumeInfo.title;
     this.author = bookdata.volumeInfo.authors;
-    this.isbn = bookdata.volumeInfo.industryIdentifiers[0].type + bookdata.volumeInfo.industryIdentifiers[0].identifier
+    this.isbn = (bookdata.volumeInfo.industryIdentifiers === undefined) ? "NO ISBN" : bookdata.volumeInfo.industryIdentifiers[0].type + bookdata.volumeInfo.industryIdentifiers[0].identifier
     this.description = bookdata.volumeInfo.description
 }
 
