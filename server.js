e4c2388b30d4acdf0a726b46d1d44d74c03b6e91
hamzashamoun96 <hamza.shamoun96@gmail.com>
2021-06-19 22:45:51 +0300

    edit

--- server.js
+++ server.js
@@ -10,8 +10,8 @@ server.use(cors());
 
 
 const pg = require('pg')
-const client = new pg.Client(process.env.DATABASE_URL)
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
+// const client = new pg.Client(process.env.DATABASE_URL)
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL, ssl: { rejectUnauthorized: false } });
 
 const superagent = require('superagent');
 
